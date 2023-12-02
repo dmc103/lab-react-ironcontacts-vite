6b9e52b3f04e7b9dce024765f016ddc3ec0c6561
Doreen <doreenmarie_cee@hotmail.com>
2023-12-02 16:48:36 +0100

    completing iterations 1-3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,7 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": "warn",
     "react/prop-types": 0,
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import "./App.css";
+import { useState } from 'react';
+import contactsInfo from "./contacts.json";
+
 
 function App() {
+  const [contacts] = useState(contactsInfo.slice(0, 5));
+
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
+    <div>
+      <h1>IronContacts</h1>
+      <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map((contact) => (
+          <tr key={contact.id}>
+            <td>
+              <img src={contact.pictureUrl} alt={contact.name} />
+            </td>
+
+            <td>{contact.name}</td>
+            <td>{contact.popularity.toFixed(2)}</td>
+            <td>{contact.wonOscar ? '🏆' : ''}</td>
+            <td>{contact.wonEmmy ? '🌟' : ''}</td>
+
+
+          </tr>
+        )
+
+        )}
+
+      
+      </tbody>
+
+
+      </table>
     </div>
+    
   );
 }
 
