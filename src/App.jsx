db4f7aae00f28787807dcc01b4ecf8511ad92a86
Doreen <doreenmarie_cee@hotmail.com>
2023-12-05 12:27:21 +0100

    completing iterations

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,49 @@ import contactsInfo from "./contacts.json";
 
 
 function App() {
-  const [contacts] = useState(contactsInfo.slice(0, 5));
+  const [contacts, setContacts] = useState(contactsInfo.slice(0, 5));
+  const contactsLeft = useState(contactsInfo.slice(0, 5));
+
+
+  const addRandomContact = () => {
+    const availContacts = contactsInfo.filter(
+      (contact) => !contacts.some((displayedContact) => displayedContact.id === contact.id)
+    );
+
+     if(contactsLeft.length > 0) {
+       const randomContact = availContacts[Math.floor(Math.random() * availContacts.length)];
+       setContacts([...contacts, randomContact]);
+       setContacts(contact => [...contact, randomContact]);
+     }
+    };
+
+  const sortByName = () => {
+    const sortedContacts = [...contacts].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    setContacts(sortedContacts);
+
+  };
+
+
+  const sortByPopularity = () => {
+    const sortedContacts = [...contacts].sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    setContacts(sortedContacts);
+  }
+
+
+
+
+
 
   return (
     <div>
       <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
       <table>
       <thead>
         <tr>
